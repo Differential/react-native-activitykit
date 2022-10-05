e54a15e6fc22e42e09f035df43f71985c503aed4
Caleb Panza <45076058+calebpanza@users.noreply.github.com>
2022-10-05 12:46:06 -0700

    check in

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  ReactNativeActivityKitExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-activitykit': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
