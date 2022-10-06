0f9d7fbcb860f721ef1e969d881f979f996d7c61
Brandon Kraeling <bkraeling@gmail.com>
2022-10-06 16:53:22 -0700

    Fix up order page

--- example/src/screens/Order.tsx
+++ example/src/screens/Order.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
     height: 60,
     marginVertical: 20,
   },
+  image: {
+    width: '100%',
+    height: 200,
+  },
 });
+
+const PIZZA_CLOSEUP =
+  'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60';
 
 type PizzaProps = {
   _id: string;
@@ -41,6 +47,7 @@ const Pizza = ({
   const result = 'You Made It To The Order Page!';
   return (
     <View style={styles.container}>
+      <Image source={{ uri: PIZZA_CLOSEUP }} style={styles.image} />
       <Text>{title}</Text>
       {/* <Button title="Order" size="large" />
       {options.map((option) => (
