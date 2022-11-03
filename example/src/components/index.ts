827a623e5457ea99c75cd6f695d933654b23bb25
Brandon Kraeling <bkraeling@gmail.com>
2022-11-03 14:39:52 -0400

    Remove pager

--- example/src/components/index.ts
+++ example/src/components/index.ts
@@ -3,6 +3,5 @@ import CartButton from './CartButton';
 import Checkbox from './Checkbox';
 import Header from './Header';
 import HelperBox from './HelperBox';
-import Pager, { Page as PageType } from './Pager';
 
-export { Button, CartButton, Checkbox, Header, HelperBox, Pager, PageType };
+export { Button, CartButton, Checkbox, Header, HelperBox };
