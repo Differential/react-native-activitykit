53bf999e1324dba8782bc5ebeb1f5617bf3e73c6
Caleb Panza <45076058+calebpanza@users.noreply.github.com>
2022-11-27 16:16:03 -0500

    add esdoc styled comments

--- src/index.tsx
+++ src/index.tsx
@@ -22,6 +22,23 @@ export function multiply(a: number, b: number): Promise<number> {
   return ReactNativeActivityKit.multiply(a, b);
 }
 
+/**
+ * Starts a Live Activity. Wrapper for the Activity.request method from ActivityKit
+ * @see https://developer.apple.com/documentation/activitykit/activity/request(attributes:contentstate:pushtype:)
+ * 
+ * @param {!Object} contentState - An object that contains the dynamic content of a Live Activity
+ * @param {!Object} [attributes={}] - An object that contains the static content of a Live Activity
+ * @returns {Promise<Object>} Activity that was started
+ *
+ * @todo add support for specifying a PushType
+ *
+ * @example <caption>Starting a Live Activity with dynamic and static content</caption>
+ * const activity = await startActivity({ status: "Dynamic Status" }, { label: "My Static Label "})
+ * 
+ * @example <caption>Starting a Live Activity with dynamic content only</caption>
+ * const activity = await startActivity({ status: "Dynamic Status" })
+ * 
+ */
 export function startActivity(
   contentState: Record<string, unknown>,
   attributes: Record<string, unknown> = {}
@@ -38,6 +55,19 @@ export function startActivity(
   });
 }
 
+/**
+ * Updates an existing activity. Wrapper for the Activity.update method from ActivityKit
+ * @see https://developer.apple.com/documentation/activitykit/activity/update(using:)
+ * 
+ * @param {!string} identifier - Identifier of the Activity that you want to update. Provided in the return value of the {@link startActivity} method
+ * @param {!Object} contentState - Content that you want to display when updating your Activity
+ * @returns {Promise<Object>} 
+ * 
+ * @todo add support for specifying an AlertConfiguration
+ * 
+ * @example <caption>Update an existing Activity</caption>
+ * const newActivity = await updateActivity("activity-id", { status: "My New Status" })
+ */
 export function updateActivity(
   identifier: string,
   contentState: Record<string, unknown>
@@ -54,9 +84,33 @@ export function updateActivity(
   });
 }
 
-// todo : I think that dismissalPolicies should be an enum. Gotta figure out how to handle that in TypeScript
-// todo : We still need to handle a final state object being passed in and also ending an Activity at a specific time (.date), so maybe "args" could be a better option here instead of a single prop?
-export function endActivity(identifier: string, options?: EndActivityOptions): Promise<ActivityKitActivity> {
+/**
+ * Ends an active Live Activity. Wrapper for the Activity.end method from ActivityKit
+ * @see https://developer.apple.com/documentation/activitykit/activity/end(using:dismissalpolicy:)
+ * 
+ * @param {!string} identifier 
+ * @param {?Object} options - Options to end the Activity with
+ * @param {?string} [options.dismissalPolicy="default"] - Specifies the Activity DismissalPolicy that you'd like to end your Activity with
+ * @param {?Object} options.finalContentState - The final piece of dynamic content to display on your Activity after it's ended. Note, this will not display if the `dismissalPolicy` is set to "immediate"
+ * @returns {Promise<Object>} 
+ * 
+ * @todo add support for the `afterDate` DismissalPolicy
+ * 
+ * @example <caption>End an Activity immediately</caption>
+ * const finalActivityState = await endActivity("activity-id", { dismissalPolicy: "immediate" })
+ * 
+ * @example <caption>End an Activity with a final content state</caption>
+ * const finalActivityState = await endActivity("activity-id", {
+ *  dismissalPolicy: "default",
+ *  finalContentState: {
+ *    status: "My Final Status"
+ *  }
+ * })
+ */
+export function endActivity(
+  identifier: string,
+  options?: EndActivityOptions
+): Promise<ActivityKitActivity> {
   const dismissalPolicy = options?.dismissalPolicy || "default"
   const finalContentState = options?.finalContentState ? JSON.stringify(options.finalContentState) : ""
 
@@ -69,6 +123,26 @@ export function endActivity(identifier: string, options?: EndActivityOptions): P
   });
 }
 
+/**
+ * Ends all currently active Activities. There is no ActivityKit equivilant, but instead this is a method provided by react-native-activitykit for convenience. Best used when trying to wipe/clean up data.
+ * 
+ * @param {?Object} options - The options that will be used when ending each Activity
+ * @returns {Promise<Array<Object>>} An array of each Activity that was ended.
+ * 
+ * @example <caption>End all Activities with default options</caption>
+ * const activities = await endActivities()
+ * 
+ * @example <caption>End all Activities immediately</caption>
+ * const activities = await endActivities({ dismissalPolicy: "immediate" })
+ * 
+ * @example <caption>End all Activities with the same final Content State</caption>
+ * const activities = await endActivities({
+ *  dismissalPolicy: "default",
+ *  finalContentState: {
+ *    status: "Final Status shared by ALL Activities"
+ *  }
+ * })
+ */
 export function endActivities(options?: EndActivityOptions): Promise<ActivityKitActivity[]> {
   const dismissalPolicy = options?.dismissalPolicy || "default"
   const finalContentState = options?.finalContentState ? JSON.stringify(options.finalContentState) : ""
@@ -83,6 +157,12 @@ export function endActivities(options?: EndActivityOptions): Promise<ActivityKit
   });
 }
 
+/**
+ * String keys for ActivityUIDismissalPolicy values
+ * @see https://developer.apple.com/documentation/activitykit/activityuidismissalpolicy
+ * 
+ * @type {{ default: string, immediate: string, afterDate: string }}
+ */
 export const ActivityDismissalPolicies: Record<string, ActivityDismissalPolicy> = {
   default: 'default',
   immediate: 'immediate',
