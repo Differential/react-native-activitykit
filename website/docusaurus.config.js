7c830afc9345c4e3612a7dd76cf50f01fb168f41
Caleb Panza <45076058+calebpanza@users.noreply.github.com>
2023-01-02 14:59:31 -0500

    Add Differential copyright

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Differential Dev Shop, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
